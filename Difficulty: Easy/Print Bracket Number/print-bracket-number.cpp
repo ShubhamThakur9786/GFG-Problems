7ff8806307d2117dcb909d95d426c294768db1fa
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-02-23 10:43:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Bracket Number/print-bracket-number.cpp
@@ -0,0 +1,24 @@
+// User function template for C++
+class Solution {
+  public:
+
+    vector<int> bracketNumbers(string str) {
+        stack<int> st;
+        vector<int> ans;
+        int count = 0;
+        
+        for(int i=0; i<str.size(); i++) {
+            if(str[i] == '(') {
+                count++;
+                ans.push_back(count);
+                st.push(count);
+            }
+            else if (str[i] == ')') {
+                ans.push_back(st.top());
+                st.pop();
+            }
+        }
+        
+        return ans;
+    }
+};

214841713205c7b6cfb45866788b7e33e2598d16
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-02-25 09:39:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.cpp
@@ -0,0 +1,47 @@
+class Solution {
+  public:
+    int getMaxArea(vector<int> &heights) {
+        // code here
+        int n = heights.size();
+        vector<int> rightSmallest(heights.size());
+        vector<int> leftSmallest(heights.size());
+
+        stack<int> st;
+
+        for(int i=0; i<n; i++) {
+            while(!st.empty() && heights[st.top()] > heights[i]) {
+                //found smallest elemnt store index of this elem in rightSmallest;
+                rightSmallest[st.top()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()) {
+            rightSmallest[st.top()] = n;
+            st.pop();
+        }
+
+        for(int i=n-1; i>=0; i--) {
+            while(!st.empty() && heights[st.top()] > heights[i]) {
+                leftSmallest[st.top()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()) {
+            leftSmallest[st.top()] = -1;
+            st.pop();
+        }
+
+        int maxi = 0;
+        for(int i=0; i<n; i++) {
+            int area = heights[i]*(rightSmallest[i] - leftSmallest[i] - 1);
+            maxi = max(maxi, area);
+        }
+
+        return maxi;
+
+    }
+};

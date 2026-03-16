5e5f900df681d7f84b2926b49dcb856b4e63373c
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-03-16 20:02:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
@@ -0,0 +1,42 @@
+// User function template for C++
+class Solution {
+  public:
+    int search(string &pat, string &txt) {
+        // code here
+        unordered_map<char, int> mp;
+        for(auto it : pat) {
+            mp[it]++;
+        }
+        int cnt = mp.size();
+        int i=0;
+        int j=0;
+        int ans = 0;
+        int k=pat.size();
+        
+        while(j < txt.size()) {
+            //calculations
+            if(mp.find(txt[j]) != mp.end()) {
+                mp[txt[j]]--;
+                if(mp[txt[j]] == 0) {
+                    cnt--;
+                }
+            }
+            
+            if(j-i+1 < k) j++;
+            else if(j-i+1 == k) {
+                if(cnt == 0) {
+                    ans++;
+                }
+                if(mp.find(txt[i]) != mp.end()) {
+                    mp[txt[i]]++;
+                    if(mp[txt[i]] == 1) {
+                        cnt++;
+                    }
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};

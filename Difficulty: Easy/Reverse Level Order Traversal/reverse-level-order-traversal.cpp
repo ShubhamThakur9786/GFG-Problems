9d027b81296ec0b1a5af17c61a55d1198963871f
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-04-07 19:31:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Level Order Traversal/reverse-level-order-traversal.cpp
@@ -0,0 +1,34 @@
+/*
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+class Solution {
+  public:
+    vector<int> reverseLevelOrder(Node *root) {
+        // code here
+        vector<int> ans;
+        stack<int> st;
+        if(root == NULL) return ans;
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()) {
+            Node* temp = q.front();
+            q.pop();
+            
+            st.push(temp->data);
+            
+            if(temp->right) q.push(temp->right);
+            if(temp->left) q.push(temp->left);
+        }
+        
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};

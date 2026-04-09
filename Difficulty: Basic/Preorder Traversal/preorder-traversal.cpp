0ae80aa3577c4b2bea930710e882be4cfd36e531
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-04-09 18:01:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Preorder Traversal/preorder-traversal.cpp
@@ -0,0 +1,32 @@
+/*
+class Node
+{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> preOrder(Node* root) {
+        // code here
+        stack<Node*> st;
+        vector<int> ans;
+        st.push(root);
+        Node* temp;
+        while(!st.empty()) {
+            temp = st.top();
+            st.pop();
+            ans.push_back(temp->data);
+            if(temp->right) st.push(temp->right);
+            if(temp->left) st.push(temp->left);
+        }
+        return ans;
+    }
+};

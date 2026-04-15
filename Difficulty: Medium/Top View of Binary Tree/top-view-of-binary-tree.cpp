afd99bc2b658c711a69c414c60252cda7bd9dec1
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-04-15 10:35:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -15,45 +15,73 @@ class Node {
 
 class Solution {
   public:
-    void findPos(Node* root, int pos, int &l, int &r) {
-        if(!root) return;
+    // void findPos(Node* root, int pos, int &l, int &r) {
+    //     if(!root) return;
         
-        l = min(l, pos);
-        r = max(r, pos);
+    //     l = min(l, pos);
+    //     r = max(r, pos);
         
-        findPos(root->left, pos-1, l, r);
-        findPos(root->right, pos+1, l, r);
-    }
+    //     findPos(root->left, pos-1, l, r);
+    //     findPos(root->right, pos+1, l, r);
+    // }
+    // vector<int> topView(Node *root) {
+    //     // code here
+    //     int l = 0;
+    //     int r = 0;
+    //     findPos(root,0,l,r);
+    //     vector<int> ans(r-l+1);
+    //     vector<int> vis(r-l+1, 0);
+    //     if(!root) return ans;
+    //     queue<Node*> q;
+    //     queue<int> index;
+    //     q.push(root);
+    //     index.push(0-l);
+        
+    //     while(!q.empty()) {
+    //         Node* temp = q.front();
+    //         q.pop();
+    //         int idx = index.front();
+    //         index.pop();
+    //         if(!vis[idx]) {
+    //             ans[idx] = temp->data;
+    //             vis[idx] = 1;
+    //         }
+    //         if(temp->left) {
+    //             q.push(temp->left);
+    //             index.push(idx-1);
+    //         }
+    //         if(temp->right) {
+    //             q.push(temp->right);
+    //             index.push(idx+1);
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
     vector<int> topView(Node *root) {
         // code here
-        int l = 0;
-        int r = 0;
-        findPos(root,0,l,r);
-        vector<int> ans(r-l+1);
-        vector<int> vis(r-l+1, 0);
+        vector<int> ans;
         if(!root) return ans;
-        queue<Node*> q;
-        queue<int> index;
-        q.push(root);
-        index.push(0-l);
+        map<int, int> mp;
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
         
         while(!q.empty()) {
-            Node* temp = q.front();
+            auto temp = q.front();
             q.pop();
-            int idx = index.front();
-            index.pop();
-            if(!vis[idx]) {
-                ans[idx] = temp->data;
-                vis[idx] = 1;
+            Node* node = temp.first;
+            int idx = temp.second;
+            
+            if(mp.find(idx) == mp.end()) {
+                //not found
+                mp[idx] = node->data;
             }
-            if(temp->left) {
-                q.push(temp->left);
-                index.push(idx-1);
-            }
-            if(temp->right) {
-                q.push(temp->right);
-                index.push(idx+1);
-            }
+            
+            if(node->left) q.push({node->left, idx-1});
+            if(node->right) q.push({node->right, idx+1});
+        }
+        for(auto i : mp) {
+            ans.push_back(i.second);
         }
         return ans;
     }
